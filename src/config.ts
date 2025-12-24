f2635607a1b84bfd6ec87a3c6277470b8481bafb
Assem Minsizbayeva <assem.minsizbayeva_2028@ucentralasia.org>
2025-12-24 16:09:34 +0500

    Add dependencies: axios, router, icons

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const MOCKAPI_BASE_URL = 'https://694bbe69da5ddabf003512e0.mockapi.io';
+
+export const SPOONACULAR_BASE_URL = 'https://api.spoonacular.com';
+
+export const SPOONACULAR_API_KEY = '70060d29184a446598ce6e8e28ff4f5e';
